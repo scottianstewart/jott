a5876ab106c6dbf244a1d7c68550832518528f74
scott stewart <scott@synapsestudios.com>
2017-05-26 17:15:53 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Jott</h2>
-        </div>
-        <p className="App-intro">
-          Get ready to "Jott" some things down
-        </p>
+      <div>
+        <h1>Jott</h1>
       </div>
     );
   }
